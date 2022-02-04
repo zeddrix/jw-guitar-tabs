220f565818dde74810af239073ca867c3188cd1a
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2022-02-04 13:31:08 +0800

    Msg when favs & history list is empty.

--- src/components/pages/FavoritesPage.svelte
+++ src/components/pages/FavoritesPage.svelte
@@ -13,7 +13,11 @@
 
 <h1>Favorites</h1>
 
-{#if $favorites}
+{#if $favorites.length == 0}
+	<p style="text-align: center; color: grey">
+		Your favorite songs will show here. Please add one...
+	</p>
+{:else}
 	{#each $favorites as fav (fav.link)}
 		<FavoriteTitleItem {fav} />
 	{/each}
--- src/components/pages/HistoryPage.svelte
+++ src/components/pages/HistoryPage.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import ComingSoon from '../layout/ComingSoon.svelte';
+	let history = [];
 </script>
 
 <h1>History</h1>
 
-<ComingSoon />
+{#if history.length == 0}
+	<p style="text-align: center; color: grey">
+		You have no recent activites. Start browsing, play songs, and enjoy!
+	</p>
+{:else}
+	{#each history as his (his.link)}
+		<!-- <HistoryTitleItem {his} /> -->
+	{/each}
+{/if}
